d5a8311dfb509b99234b2626da3e50919ae935f6
sebug <sebastian.gfeller@gmail.com>
2018-04-28 19:34:13 +0200

    With some logging

--- SearchBoxesTrigger/index.js
+++ SearchBoxesTrigger/index.js
@@ -4,7 +4,7 @@ module.exports = function (context, req) {
     const urlTemplate = 'https://swisspost.opendatasoft.com/api/records/1.0/search/?dataset=zugangspunkte-post&facet=address_kantoncode&facet=address_zip&facet=poityp_de&facet=service_de&refine.poityp_de=Briefeinwurf';
     context.log('searching for ' + req.query.city);
 
-    https.get(urlTemplate + '?q=' + req.query.city, (resp) => {
+    https.get(urlTemplate + '&q=' + req.query.city, (resp) => {
 	let data = '';
 
 	resp.on('data', (chunk) => {
@@ -13,6 +13,9 @@ module.exports = function (context, req) {
 	
 	resp.on('end', () => {
 	    let boxesResult = JSON.parse(data);
+
+	    context.log('at end');
+	    context.log('got ' + boxesResult.records.length + ' results');
 
 	    context.res = {
 		body: boxesResult.records
