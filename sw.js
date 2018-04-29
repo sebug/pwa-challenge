b96e83161e5fc450fee71779426129cc6f620016
sebug <sebastian.gfeller@gmail.com>
2018-04-29 12:34:41 +0200

    With static cache

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ var urlsToCache = [
     '/static/require.min.js',
     '/static/dist.js',
     '/static/knockout-3.4.2.js',
-    '/static/jquery-3.2.1.min'
+    '/static/jquery-3.2.1.min.js'
 ];
 
 
@@ -16,5 +16,19 @@ self.addEventListener('install', function (e) {
 	    .then(function (cache) {
 		console.log('Opened cache ' + CACHE_NAME);
 		return cache.addAll(urlsToCache);
+	    }, function (err) {
+		console.log('could not event open cache ' + CACHE_NAME);
+	    }));
+});
+
+// Deal with statically cached content
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+	caches.match(e.request)
+	    .then(function (response) {
+		if (response) {
+		    return response;
+		}
+		return fetch(e.request);
 	    }));
 });
